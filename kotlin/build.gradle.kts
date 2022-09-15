42998f6abe8730b5e92371b4b5aa4c0ffd051bd6
Nicolas Peron <nperon@gmail.com>
2022-09-15 11:22:00 +0200

    build: add sonatype publishing tasks

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,5 +1,9 @@
 import com.google.protobuf.gradle.*
 
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
 buildscript {
     repositories {
         maven {
@@ -109,6 +113,15 @@ subprojects {
                     password = project.findProperty("maven.credentials.password") as String?
                 }
             }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
