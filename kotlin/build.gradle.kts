7afc966e8c4671019ca7bdd3fa4edf6acc80ce42
Nicolas Peron <nperon@gmail.com>
2022-09-16 19:52:52 +0200

    build: configure repository uri for release

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -119,9 +119,12 @@ subprojects {
 
 nexusPublishing {
     repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        sonatype {
+            val repositoryUrl = project.findProperty("repositoryUrl")
+            repositoryUrl?.let{
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri(repositoryUrl))
+            }
         }
     }
 }
